059c2e2d77979ba3ae12ee58aafe2d4495251a51
Parnell Springmeyer <parnell@ixmat.us>
2014-12-16 13:34:09 -0600

    Better debug msgs

--- src/erls3server.erl
+++ src/erls3server.erl
@@ -181,7 +181,7 @@ handle_cast(_Msg, State) ->
 %% Description: Handling all non call/cast messages
 %%--------------------------------------------------------------------
 handle_info({ibrowse_async_headers,RequestId,Code,Headers },State = #state{pending=P}) ->
-        ?DEBUG("******* Headers :  ~p~n", [Headers]),
+        ?DEBUG("******* Headers :  ~w ~p~n", [Code, Headers]),
 	case gb_trees:lookup(RequestId,P) of
 		{value,#request{pid = Pid }=R} ->
 		    {ICode, []} = string:to_integer(Code),
@@ -390,20 +390,20 @@ genericRequest(From, #state{ssl=SSL, access_key=AKI, secret_key=SAK, region=Regi
                 {ok, Code, _H, _B} ->
                    {reply, {error, return_code, Code}, State};
                 {error, retry_later} ->
-                    {reply, {error, "retry_later", "Error Occured"}, State};
-                    %%{reply, retry, State};
+                    ?DEBUG("******* ERROR : retry_later \n",[]),
+                    {reply, retry, State};
                 {error, conn_failed} ->
-                    {reply, {error, "conn_failed", "Error Occured"}, State};
-                    %%{reply, retry, State};
-                {error, {conn_failed, _}} ->
-                    {reply, {error, "conn_failed more", "Error Occured"}, State};
-                    %%{reply, retry, State};
+                    ?DEBUG("******* ERROR : conn_failed \n",[]),
+                    {reply, retry, State};
+                {error, {conn_failed, Why}} ->
+                    ?DEBUG("******* ERROR : conn_failed ~w\n", [Why]),
+                    {reply, retry, State};
                 {error, {send_failed, _}} ->
-                    {reply, {error, "send_failed", "Error Occured"}, State};
-                    %%{reply, retry, State};
+                    ?DEBUG("******* ERROR : send_failed \n",[]),
+                    {reply, retry, State};
                 {error, req_timedout} ->
-                    {reply, {error, "req_timedout", "Error Occured"}, State};
-                    %%{reply, retry, State};
+                    ?DEBUG("******* ERROR :  req_timedout \n",[]),
+                    {reply, retry, State};
                 {error, E} ->
                     {reply, {error, E, "Error Occured"}, State}
             end
