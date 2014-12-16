09ca2d1527d39f18aa2e346333e8beca1046fc79
Parnell Springmeyer <parnell@ixmat.us>
2014-12-16 02:32:28 -0600

    Hope this fixes it

--- src/erls3util.erl
+++ src/erls3util.erl
@@ -12,7 +12,7 @@
 -include_lib("xmerl/include/xmerl.hrl").
 
 region_url("us-east-1") -> "s3.amazonaws.com";
-region_url(R) -> io_lib:format("s3-~s.amazonaws.com", [R]).
+region_url(R) -> ?FORMAT("s3-~s.amazonaws.com", [R]).
     
 %% Collapse equal keys into one list
 consume ({K,V}, [{K,L}|T]) -> [{K,[V|L]}|T];
